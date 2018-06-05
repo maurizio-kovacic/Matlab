04b2a39300fdfc66c9e84131cb6d4465d59e5f68
maurizio-kovacic <maurizio-kovacic@users.noreply.github.com>
2018-06-05 16:16:37 +0200

    Add files via upload

--- Wrapper/plane3.m
+++ Wrapper/plane3.m
@@ -22,12 +22,17 @@ if( nargin < 3 )
     B = [];
 end
 if( isempty(B) )
-    F = eye(3);
-    R = reshape(RUt(normr(cross([0 0 1],T,2)),dot([0 0 1],T,2)),3,3);
-    F = R*F;
-    T = F(:,1)';
-    B = F(:,2)';
-
+    an     = acos(dot(repmat([0 0 1],size(T,1),1),T,2));
+    F      = repmat({eye(3)},size(P,1),1);
+    R      = F;
+    i      = find(an>0);
+    if( ~isempty(i) )
+        R(i) = squeeze(num2cell(axang2rotm([normr(cross(repmat([0 0 1],numel(i),1),T(i,:),2)) an(i,:)]),[1 2]));
+        F    = R.*F;
+    end
+    F      = cell2mat(cellfun(@(M) M(1:9),F,'UniformOutput',false));
+    T      = F(:,1:3);
+    B      = F(:,4:6);
 end
 T = side * T;
 B = side * B;
