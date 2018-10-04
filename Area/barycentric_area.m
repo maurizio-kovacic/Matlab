8095b9066fd3870aa94ef1ab188c0266cdbdd14b
maurizio-kovacic <maurizio-kovacic@users.noreply.github.com>
2018-10-04 14:47:22 +0200

    Add files via upload

--- Area/barycentric_area.m
+++ Area/barycentric_area.m
@@ -1,8 +1,6 @@
 function [ A ] = barycentric_area( P, T )
 n = size(P,1);
-I = T(:,1);
-J = T(:,2);
-K = T(:,3);
+[I,J,K] = tri2ind(T);
 a = triangle_area( P, T ) / 3;
 A = sparse( [I;J;K], [I;J;K], [a;a;a], n, n );
 end
